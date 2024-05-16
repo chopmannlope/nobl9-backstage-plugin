26df2f4dd87a3debfdde573807ac89bdbfd2893c
Cornelio Hopmann <cornelio.hopmann.lopez@zalando.de>
2024-05-16 09:50:21 +0200

    fix credentials: include

--- plugins/nobl9-plugin/src/components/SloPage/SloPage.tsx
+++ plugins/nobl9-plugin/src/components/SloPage/SloPage.tsx
@@ -74,7 +74,9 @@ export const SloPage = () => {
 
   const { value, loading, error } =
     useAsync(async (): Promise<N9BackendResponse> => {
-      return await fetch(getBackendUrl(config, entity), {crendetials: "include"}).then(res => res.json());
+      return await fetch(getBackendUrl(config, entity), {
+        credentials: 'include',
+      }).then(res => res.json());
     }, [entity]);
 
   if (loading) {
